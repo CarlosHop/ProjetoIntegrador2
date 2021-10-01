ef2c039023e93a00c36f86365dac4157d1e2f661
Gabriel Nóbrega <84252664+GabrielMorrissey@users.noreply.github.com>
2021-10-01 15:02:47 -0300

    Primeiro upload dos arquivos do Projeto Integrador II.

---
+++ projetointegrador2/src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Projeto Integrador II - Tecnologia em Análise e Desenvolvimento de Sistemas
+ * Senac São Paulo - Unidade Santo Amaro - 2021
+ *
+ * Os nomes dos autores deste código estão abaixo.
+ * 
+ * Brendo Rotta
+ * Cathariny Damasceno
+ * Carlos Henrique
+ * Gabriel de Nóbrega
+ */
+package main;
+
+import methods.Methods;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Methods met = new Methods();
+        
+        met.boasVindas();
+    }
+
+}
---
+++ projetointegrador2/src/main/java/methods/Methods.java
@@ -0,0 +1,9 @@
+package methods;
+
+public class Methods {
+    
+    public void boasVindas() {
+        System.out.println("*-- Bem-vindo(a) a nosso sistemas de cadastro! --*");
+    }
+    
+}
