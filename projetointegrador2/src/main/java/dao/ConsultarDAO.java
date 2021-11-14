efeae131573b401bb8aadd27bb76652600f11cff
Brendo Rotta <brendorotta16@gmail.com>
2021-11-14 16:49:33 -0300

    Iniciando a consulta DAO

--- projetointegrador2/src/main/java/dao/ConsultarDAO.java
+++ projetointegrador2/src/main/java/dao/ConsultarDAO.java
@@ -1,11 +1,21 @@
 package dao;
 
-
-
-
-
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 // Para realizar consultas
 public class ConsultarDAO {
+    // Criando conexão com o banco de dados
+    public static String url = "jdbc:mysql://localhost:3306/Filira_essence" + "?useTimezone=true&serverTimezone=UTC&useSSL=false";
+    public static String login = "root";
+    public static String senha = "";
+    
+    // Criar objeto de conexão
+    Connection conexao = null;
+    
+
+    //==========================================================================
+            // Funções para puxar informações no banco de dados
     
 }
